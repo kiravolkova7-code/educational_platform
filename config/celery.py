5fdd698512e650ef4b7cb2d6ba50f67fdc7589ab
august <kira_volkova_7@mail.ru>
2026-08-03 01:02:35 +0500

    help00

--- config/celery.py
+++ config/celery.py
@@ -2,7 +2,6 @@ from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 
-
 # Установка переменной окружения для настроек проекта
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
